10c9ca1f31446b644890b4322f99ac79b664d05c
Dzmitry Hutaryan <dmitry.gutoryan@gmail.com>
2023-06-01 23:11:58 +0300

    docs: init demo-app

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -6,13 +6,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatNativeDateTimeModule, MatTimepickerModule } from 'mat-timepicker';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -20,10 +25,14 @@ import { AppComponent } from './app.component';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatCardModule,
     MatTimepickerModule,
     MatNativeDateTimeModule,
     FormsModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
     AppRoutingModule,
   ],
   providers: [],
