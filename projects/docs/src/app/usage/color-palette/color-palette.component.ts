e4aa2e291d517349b8713c988740da817e8d30c3
Dzmitry Hutaryan <dzmitry.hutaryan@gmail.com>
2024-04-19 21:31:50 +0200

    deps: bump libs versions

--- projects/docs/src/app/usage/color-palette/color-palette.component.ts
+++ projects/docs/src/app/usage/color-palette/color-palette.component.ts
@@ -3,6 +3,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'docs-color-palette',
   templateUrl: './color-palette.component.html',
+  styles: `
+    .mat-mdc-form-field {
+      display: block;
+      width: max-content;
+    }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ColorPaletteComponent {}
