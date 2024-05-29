6c01b4e8e7c5abda9ed738bf30ab1f2a21c8430a
Dzmitry Hutaryan <dzmitry.hutaryan@gmail.com>
2024-05-29 23:00:36 +0200

    feat(adapter): add provider function for native date time

--- projects/mat-timepicker/src/lib/adapter/index.ts
+++ projects/mat-timepicker/src/lib/adapter/index.ts
@@ -1,5 +1,5 @@
 import { PlatformModule } from '@angular/cdk/platform';
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Provider } from '@angular/core';
 
 import { MAT_TIME_LOCALE_PROVIDER, TimeAdapter } from './time-adapter';
 import { NativeDateTimeAdapter } from './native-date-time-adapter';
@@ -9,9 +9,15 @@ export * from './native-date-time-adapter';
 
 @NgModule({
   imports: [PlatformModule],
-  providers: [
-    { provide: TimeAdapter, useClass: NativeDateTimeAdapter },
-    MAT_TIME_LOCALE_PROVIDER,
-  ],
+  providers: [{ provide: TimeAdapter, useClass: NativeDateTimeAdapter }],
+})
+export class NativeDateTimeModule {}
+
+@NgModule({
+  providers: [provideNativeDateTimeAdapter()],
 })
 export class MatNativeDateTimeModule {}
+
+export function provideNativeDateTimeAdapter(): Provider {
+  return { provide: TimeAdapter, useClass: NativeDateTimeAdapter };
+}
